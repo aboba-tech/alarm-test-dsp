208def6e70bbd14d17faab2526a3a9db2894e86f
aboba-tech <MNazarenko@DataSpace.local>
2025-09-04 16:41:55 +0300

    aboba commit

--- main.py
+++ main.py
@@ -11,18 +11,19 @@ import mails
 
 
 email_from = 'noreply@dataspace.ru'
-email_to = 'maxim.nazarenko@dataspace.ru'.split()
+email_to = 'maxim.nazarenko@dataspace.ru'
 # email_blind_carbon_copy = 'maxim.nazarenko@dataspace.ru'
 email_smtp_server = '192.168.150.55'
 
 
 choice = ch(mails.temp_list)
 text = str(list(choice.values())[1])
+header = str(list(choice.values())[0])
 
 msg = MIMEMultipart()
 msg['From'] = email_from
 msg['To'] = email_to
-msg['Subject'] = Header(list(choice.values())[0], 'utf-8')
+msg['Subject'] = Header(header, 'utf-8')
 
 
 
@@ -35,17 +36,17 @@ msg.attach(MIMEText(text, 'plain', 'utf-8'))
 # lines.append('mesg')
 
 # msg = "\r\n".join(lines)
-print(text)
+print(text, type(text))
 
 
-# try:
-#     server = smtplib.SMTP(email_smtp_server)
-#     server.set_debuglevel(1)
-#     server.sendmail(email_from, email_to, msg.as_string())
+try:
+    server = smtplib.SMTP(email_smtp_server)
+    server.set_debuglevel(1)
+    server.sendmail(email_from, email_to, msg.as_string())
 
-# # except Exception as e:
-# #     print(f'Возникла ошибка {e}')
+# except Exception as e:
+#     print(f'Возникла ошибка {e}')
 
-# finally:
-#     if 'server' in locals() and server:
-#         server.quit()
+finally:
+    if 'server' in locals() and server:
+        server.quit()
